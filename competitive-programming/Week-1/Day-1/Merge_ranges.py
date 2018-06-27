e28e58951466a8a631f7316a155f11e4211c42a3
Surya Deepthi Rajaparthi <31200275+SuryaDeepthiR@users.noreply.github.com>
2018-06-27 09:11:35 +0530

    Create Merge_ranges.py

---
+++ competitive-programming/Week-1/Day-1/Merge_ranges.py
@@ -0,0 +1,61 @@
+import unittest
+from operator import itemgetter
+
+def merge_ranges(temp_tuple):
+    if len(temp_tuple) <= 1:
+        return temp_tuple
+
+    temp_tuple = sorted(temp_tuple, key=itemgetter(0))
+    previous = [temp_tuple[0]]
+    temp_tuple = temp_tuple[1:]
+    for (current_first, current_last) in temp_tuple:
+        (previous_first, previous_last) = previous[-1]
+        if current_first <= previous_last:
+            if previous_last <= current_last:
+                previous_last = current_last
+            previous[-1] = (previous_first, previous_last)
+        else:
+            previous.append((current_first, current_last))
+    return previous
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_meetings_overlap(self):
+        actual = merge_ranges([(1, 3), (2, 4)])
+        expected = [(1, 4)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_touch(self):
+        actual = merge_ranges([(5, 6), (6, 8)])
+        expected = [(5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meeting_contains_other_meeting(self):
+        actual = merge_ranges([(1, 8), (2, 5)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_stay_separate(self):
+        actual = merge_ranges([(1, 3), (4, 8)])
+        expected = [(1, 3), (4, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_multiple_merged_meetings(self):
+        actual = merge_ranges([(1, 4), (2, 5), (5, 8)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_not_sorted(self):
+        actual = merge_ranges([(5, 8), (1, 4), (6, 8)])
+        expected = [(1, 4), (5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_sample_input(self):
+        actual = merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+        expected = [(0, 1), (3, 8), (9, 12)]
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
