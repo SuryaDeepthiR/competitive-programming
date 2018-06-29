bbf7112639a89c4dd29a217ba5e8361715100360
Surya Deepthi Rajaparthi <31200275+SuryaDeepthiR@users.noreply.github.com>
2018-06-29 12:39:14 +0530

    Create InflightEntertainment.py

---
+++ competitive-programming/Week-1/Day-5/InflightEntertainment.py
@@ -0,0 +1,50 @@
+import unittest
+
+
+def can_two_movies_fill_flight(movie_lengths, flight_length):
+  
+    list_of_movie_lengths_6seen = set()
+
+    for length_of_first_movie in movie_lengths:
+        length_of_second_movie = flight_length - length_of_first_movie
+        if length_of_second_movie in list_of_movie_lengths_seen:
+            return True
+        list_of_movie_lengths_seen.add(length_of_first_movie)
+
+    # We never found a match, so return False
+    return False
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_short_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 1)
+        self.assertFalse(result)
+
+    def test_long_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 6)
+        self.assertTrue(result)
+
+    def test_one_movie_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8], 6)
+        self.assertFalse(result)
+
+    def test_two_movies_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8, 3], 6)
+        self.assertTrue(result)
+
+    def test_lots_of_possible_pairs(self):
+        result = can_two_movies_fill_flight([1, 2, 3, 4, 5, 6], 7)
+        self.assertTrue(result)
+
+    def test_only_one_movie(self):
+        result = can_two_movies_fill_flight([6], 6)
+        self.assertFalse(result)
+
+    def test_no_movies(self):
+        result = can_two_movies_fill_flight([], 2)
+        self.assertFalse(result)
+
+
+unittest.main(verbosity=2)
