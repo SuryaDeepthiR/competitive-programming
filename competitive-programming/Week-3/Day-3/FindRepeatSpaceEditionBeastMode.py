2681052cefcde98fdd3b2737b33043816913e942
Surya Deepthi Rajaparthi <31200275+SuryaDeepthiR@users.noreply.github.com>
2018-07-12 10:10:54 +0530

    Create FindRepeatSpaceEditionBeastMode.py

---
+++ competitive-programming/Week-3/Day-3/FindRepeatSpaceEditionBeastMode.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+def find_duplicate(arr):
+
+    # Find a number that appears more than once ... in O(n) time
+    size = len(arr)
+     
+  
+    for element in arr:
+         
+        if arr[abs(element)] >= 0:
+            arr[abs(element)] = -arr[abs(element)]
+        else:
+            return abs(element)
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_just_the_repeated_number(self):
+        actual = find_duplicate([1, 1])
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_short_list(self):
+        actual = find_duplicate([1, 2, 3, 2])
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_medium_list(self):
+        actual = find_duplicate([1, 2, 5, 5, 5, 5])
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_long_list(self):
+        actual = find_duplicate([4, 1, 4, 8, 3, 2, 7, 6, 5])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
